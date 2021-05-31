df6e98d9022264a51da7bf135e949970ce3a8fa6
IrinaDruzhinina <imdruzh@gmail.com>
2021-05-31 15:21:21 +0300

    Нужно больше функций,правки

--- js/main.js
+++ js/main.js
@@ -7,16 +7,9 @@ const getRandomIntegrity = (min, max) => {
 };
 getRandomIntegrity(-1, 3);
 
-// Функция возвращает случайное целое число с плавающей точкой
-const getRandomFloat = (min, max) => {
-  const randomNum = (Math.random() * (max - min) + min).toFixed(5);
+// Функция возвращает случайное число с плавающей точкой
+const getRandomFloat = (min, max, floatNumber) => {
+  const randomNum = (Math.random() * (max - min) + min).toFixed(Math.abs(floatNumber));
   return Math.abs(randomNum);
 };
-getRandomFloat(-1, 2);
-
-
-// Для Кекстаграма:
-
-// Функция для проверки максимальной длины строки
-const getValidLength = (text, maxLength) => text.length <= maxLength;
-getValidLength('Kekstagram', 10);
+getRandomFloat(-1, 2, -5);
